8f6c8265c816a92192a73af2ba651e2cf6a76ba8
2youngjun <86882798+2youngjun@users.noreply.github.com>
2022-08-26 01:18:53 +0900

    [Feat] #1 - 단어 수기 추가 뷰 TextField target 함수 및 titleText 변경

--- Nadam/Nadam/WordList/AddWordViewController.swift
+++ Nadam/Nadam/WordList/AddWordViewController.swift
@@ -58,14 +58,17 @@ class AddWordViewController: UIViewController {
     // MARK: IBAction 함수
     @IBAction func startEditingTextField(_ sender: UITextField) {
         sender.layer.borderWidth = 0.5
-        sender.layer.borderColor = UIColor.NColor.blue.cgColor
         sender.layer.cornerRadius = 5.0
+        sender.layer.borderColor = UIColor.NColor.blue.cgColor
     }
     
     @IBAction func endEditingTextField(_ sender: UITextField) {
         sender.layer.borderWidth = 0.5
         sender.layer.borderColor = UIColor.NColor.border.cgColor
         sender.layer.cornerRadius = 5.0
+        if sender.text != "" {
+            
+        }
     }
     
     @IBAction func tapCancelButton(_ sender: UIButton) {
@@ -82,6 +85,11 @@ class AddWordViewController: UIViewController {
         self.delegate?.didSelectSaveWord()
 
         self.navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func tapOtherSpace(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+        
     }
     
     private func configureLayoutStyle() {
@@ -101,49 +109,61 @@ class AddWordViewController: UIViewController {
         duplicateSentense.font = UIFont.NFont.addWordButtonLabel
         duplicateSentense.textColor = UIColor.NColor.orange
         duplicateSentense.layer.opacity = 0
+        
+        attributeNameMeaningTitle()
+    }
+    
+    private func attributeNameMeaningTitle() {
+        let attributedNameStr = NSMutableAttributedString(string: wordName.text!)
+        let attributedMeaningStr = NSMutableAttributedString(string: wordMeaning.text!)
+        attributedNameStr.addAttribute(.foregroundColor, value: UIColor.NColor.orange, range: (wordName.text! as NSString).range(of: "•"))
+        attributedMeaningStr.addAttribute(.foregroundColor, value: UIColor.NColor.orange, range: (wordMeaning.text! as NSString).range(of: "•"))
+        
+        wordName.attributedText = attributedNameStr
+        wordMeaning.attributedText = attributedMeaningStr
     }
     
     @objc private func saveButtonState() {
-        if nameTextField.text != "" && meaningTextField.text != "" && checkSameWord() {
-            saveButton.isEnabled = true
+        if nameTextField.text != "" && meaningTextField.text != "" {
+            checkSameWord()
         } else { saveButton.isEnabled = false }
     }
     
-    private func checkSameWord() -> Bool {
+    private func checkSameWord() {
         let wordString = nameTextField.text ?? ""
         for word in wordList {
             if word.name == wordString {
                 self.ifIsSameWord()
-                return false
+                saveButton.isEnabled = false
+                return
             }
         }
-        
-        return true
+        self.ifIsNotSameWord()
+        saveButton.isEnabled = true
     }
     
     @objc private func isSameWord() {
-        let wordString = nameTextField.text
-        for word in wordList {
-            if wordString == word.name {
-                saveButton.isEnabled = false
-            }
+        if nameTextField.text != ""{
+            checkSameWord()
         }
     }
     
     private func ifIsSameWord() {
         self.duplicateSentense.layer.opacity = 1.0
-        self.nameTextField.layer.borderColor = UIColor.NColor.orange.cgColor
+//        self.nameTextField.layer.borderColor = UIColor.NColor.orange.cgColor
     }
     
-    private func isIsNotSameWord() {
+    private func ifIsNotSameWord() {
         self.duplicateSentense.layer.opacity = 0
-        self.nameTextField.layer.borderColor = UIColor.NColor.blue.cgColor
+//        self.nameTextField.layer.borderColor = UIColor.NColor.blue.cgColor
     }
     
     private func configureInputField() {
         self.nameTextField.addTarget(self, action: #selector(saveButtonState), for: .editingChanged)
+        self.nameTextField.addTarget(self, action: #selector(isSameWord), for: .editingChanged)
         self.nameTextField.addTarget(self, action: #selector(isSameWord), for: .editingDidEnd)
-//        self.nameTextField.addTarget(self, action: #selector(isSameWord), for: .allEditingEvents)
+        
+        
         self.meaningTextField.addTarget(self, action: #selector(saveButtonState), for: .editingChanged)
     }
 }
