e415ef8c01acfcd4497585ad1f7a3ad0f9920f69
2youngjun <86882798+2youngjun@users.noreply.github.com>
2022-10-02 01:28:00 +0900

    [Feat] PetView, PurchasingView 기본 레이아웃 구성

---
+++ Nadam/Nadam/Pet/PetView/PetViewController.swift
@@ -0,0 +1,36 @@
+//
+//  PetViewController.swift
+//  Nadam
+//
+//  Created by 이영준 on 2022/10/02.
+//
+
+import UIKit
+
+class PetViewController: UIViewController {
+
+    //MARK: Variable
+    
+    
+    //MARK: IBOutlet Variable
+    @IBOutlet weak var pointImage: UIImageView!
+    @IBOutlet weak var pointBackgroundView: UIView!
+    @IBOutlet weak var pointLabel: UILabel!
+    @IBOutlet weak var purchasingButton: UIButton!
+    
+    //MARK: IBOutlet Function
+    @IBAction func tapPurchasingButton(_ sender: UIButton) {
+        let storyBoard = UIStoryboard(name: "PurchasingView", bundle: nil)
+        guard let purchasingViewController = storyBoard.instantiateViewController(withIdentifier: "PurchasingViewController") as? PurchasingViewController else { return }
+        self.navigationController?.pushViewController(purchasingViewController, animated: true)
+    }
+    
+    //MARK: View Lifecycle Function
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+    
+}
---
+++ Nadam/Nadam/Pet/PurchasingView/PurchasingViewController.swift
@@ -0,0 +1,27 @@
+//
+//  PurchasingViewController.swift
+//  Nadam
+//
+//  Created by 이영준 on 2022/10/02.
+//
+
+import UIKit
+
+class PurchasingViewController: UIViewController {
+    
+    //MARK: Variable
+    
+    
+    //MARK: IBOutlet Variable
+    
+    
+    //MARK: IBOutlet Function
+    
+    
+    //MARK: View Lifecycle Function
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
